bc72faf5b701ab47104dd7a5c419fafcf49e75dc
sddd1234567 <sddd1234567@gmail.com>
2022-10-30 21:00:05 +0800

    fix modal loading problem

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react"
+
 const Modal = (props) => {
     const {children, title} = props
+
+    const [content, setContent] = useState(null)
+
+    const onModalClosed =  () => {
+      setContent(<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>)
+    }
+    
+    useEffect(() => {
+      document.querySelector('#project-modal').addEventListener('hidden.bs.modal', onModalClosed)
+      return () => {
+        document.querySelector('#project-modal').removeEventListener('hidden.bs.modal', onModalClosed)
+      };
+    }, []);
+
+    useEffect(() => {
+      setContent(children)
+    }, [children])
 
     return (
         <div className="modal fade" id="project-modal" data-bs-keyboard="true" tabIndex="-1">
@@ -10,7 +29,7 @@ const Modal = (props) => {
                 <button type="button" className="fa-solid fa-xmark close-modal-button" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div className="modal-body content-font">
-              {children}
+              {content}
               </div>
             </div>
           </div>
